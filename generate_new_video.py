6905a29109a07f12742048872422821c9c555135
ervin wang <ervin.wang.881@gmail.com>
2025-03-05 18:49:44 -0500

    generate translated video

--- generate_new_video.py
+++ generate_new_video.py
@@ -2,17 +2,17 @@ import subprocess
 import argparse
 
 def run_shell_scripts_and_python(video_file):
-    # Run the first shell script using ./ 
     print("Running translation...")
     subprocess.run(f"echo {video_file} | ./run_translate.sh", check=True, shell=True)
 
-    # Run the second shell script using ./ 
     print("Running re-synthesis...")
     subprocess.run(["./run_hift.sh"], check=True, shell=True)
 
-    # Run the Python script
     print("Running audio replacement...")
     subprocess.run(["python", "replace_audio.py"], check=True)
+
+    print("Running lipsync...")
+    subprocess.run(f"echo {video_file} | ./run_musetalk.sh", check=True, shell=True)
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Run video translation with a given file.")
