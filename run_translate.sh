819905d805538e036fd1221af959a0fb6a66ff83
ervin wang <ervin.wang.881@gmail.com>
2025-03-05 13:21:37 -0500

    scripts to run code

---
+++ run_translate.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+python3.10 -m venv heygen_env
+
+VENV_PATH="heygen_env/bin/activate"
+
+if [ -f "$VENV_PATH" ]; then
+    source "$VENV_PATH"
+    echo "Virtual environment activated."
+else
+    echo "Error: Virtual environment activate script not found."
+    exit 1
+fi
+
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
+echo "Creating 'warped_audio' folder..."
+mkdir -p warped_audio
+
+echo "Creating 'german_sentences' folder..."
+mkdir -p german_sentences
+
+python install_espeak.py 
+
+echo "Setup completed successfully!"
